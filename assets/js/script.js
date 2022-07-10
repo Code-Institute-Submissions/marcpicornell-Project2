af1736256b2af849569009402887ff0e1657a5a5
Marc Picornell <marc@marcpicornell.com>
2022-07-10 19:44:19 +0100

    Add JS functions structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,43 @@
  * Begining of JavaScript
  * Marc Picornell - 2022
  */
+
+function getPlayerName()
+{
+
+}
+
+function getComputerName()
+{
+
+}
+
+function getRandomNumber()
+{
+
+}
+
+function getAction()
+{
+
+}
+
+function displayScore()
+{
+
+}
+
+function incrementScore()
+{
+
+}
+
+function persistScore()
+{
+
+}
+
+function whoWin()
+{
+
+}
